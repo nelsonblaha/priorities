bcf18479e6a37c3cbc92987dd4bf9fe35efb3e34
Ben Nelson <liechtensteiner@protonmail.ch>
2024-02-09 09:01:31 -0700

    Buncha stuff

---
+++ README.md
@@ -0,0 +1,13 @@
+### TODO
+
+- CouchDB design docs to source control
+- Cypress
+- Easy deployment
+  - Docker Compose for CouchDB
+  - Update check
+- Contexts
+  - Home Assistant sensors
+    - UI to add selected sensors and map values
+  - Device cookie
+- Projects/prioritization
+- Use Home Assistant tasks? Deployment within Home Assistant
--- src/main.rs
+++ src/main.rs
@@ -9,9 +9,28 @@ use actix_files as fs;
 use utils::cors::cors_setup;
 use env_logger;
 use std::env;
+use serde::Deserialize;
+use std::fs::read_to_string;
+
+#[derive(Deserialize)]
+struct Config {
+    server: ServerConfig,
+}
+
+#[derive(Deserialize)]
+struct ServerConfig {
+    allowed_origin: String,
+}
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    let config_contents = read_to_string("Configuration.toml")
+        .expect("Failed to read configuration file");
+
+    let config: Config = toml::from_str(&config_contents)
+        .expect("Failed to parse configuration file");
+
+    env::set_var("ALLOWED_ORIGIN", config.server.allowed_origin);
     env::set_var("RUST_LOG", "actix_web=debug,actix_server=info");
     env_logger::init();
     HttpServer::new(move || {
